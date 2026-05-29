4d83987197694aa4518da22b6fcf9a70a3249500
Wisdom Magnus <wisdom.magnus@gmail.com>
2026-05-29 07:47:32 +0300

    Handle Android edge-to-edge display

--- android/app/src/main/kotlin/com/rhemalize/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/rhemalize/app/MainActivity.kt
@@ -1,5 +1,20 @@
 package com.rhemalize.app
 
+import android.graphics.Color
+import android.os.Bundle
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.ryanheise.audioservice.AudioServiceActivity
 
-class MainActivity : AudioServiceActivity()
+class MainActivity : AudioServiceActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        window.statusBarColor = Color.TRANSPARENT
+        window.navigationBarColor = Color.TRANSPARENT
+        WindowInsetsControllerCompat(window, window.decorView).apply {
+            isAppearanceLightStatusBars = true
+            isAppearanceLightNavigationBars = true
+        }
+        super.onCreate(savedInstanceState)
+    }
+}
